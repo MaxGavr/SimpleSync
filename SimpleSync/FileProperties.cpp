f3c89ddf8eb9360ccfb49af812baee35f6f16908
Max Gavrilyuk <max.gavr@tut.by>
2017-07-11 21:23:06 +0300

    [logic] Add file comparison functions

--- SimpleSync/FileProperties.cpp
+++ SimpleSync/FileProperties.cpp
@@ -1,6 +1,21 @@
 #include "stdafx.h"
 #include "FileProperties.h"
+#include <algorithm>
 
+
+
+
+FileProperties::FileProperties(const CString& fileName, ULONGLONG sizeInBytes,
+                               const CTime& creationTime, const CTime& accessTime, const CTime& writeTime,
+                               BOOL isDir)
+    : m_fileName(fileName),
+      m_creationTime(creationTime),
+      m_lastAccessTime(accessTime),
+      m_lastWriteTime(writeTime),
+      m_isDirectory(isDir),
+      m_sizeInBytes(sizeInBytes)
+{
+}
 
 FileProperties::FileProperties(const CString& fileName, BOOL isDir)
     : m_fileName(fileName), m_isDirectory(isDir)
@@ -10,6 +25,46 @@ FileProperties::FileProperties(const CString& fileName, BOOL isDir)
 
 FileProperties::~FileProperties()
 {
+}
+
+FileProperties::COMPARISON_RESULT FileProperties::compareTo(const FileProperties& file,
+                                                            const FileComparisonParameters& params) const
+{
+    if (this->getFileName() != file.getFileName())
+    {
+        return COMPARISON_RESULT::UNDEFINED;
+    }
+
+    ComparisonResults results;
+
+    if (params.compareSize)
+    {
+        COMPARISON_RESULT sizeCompare = compareProperty(this->getSize(), file.getSize());
+        results.push_back(sizeCompare);
+    }
+
+    if (params.compareTime)
+    {
+        COMPARISON_RESULT timeCompare;
+
+        switch (params.timeToCompare)
+        {
+        case TIMES::CREATION_TIME:
+            timeCompare = compareProperty(this->getCreationTime(), file.getCreationTime());
+            break;
+        case TIMES::LAST_ACCESS_TIME:
+            timeCompare = compareProperty(this->getLastAccessTime(), file.getLastAccessTime());
+            break;
+        case TIMES::LAST_WRITE_TIME:
+            timeCompare = compareProperty(this->getLastWriteTime(), file.getLastWriteTime());
+            break;
+        }
+
+        results.push_back(timeCompare);
+    }
+
+    return makeChoice(results);
+
 }
 
 BOOL FileProperties::operator<(const FileProperties& file) const
@@ -38,7 +93,56 @@ CString FileProperties::getFileFolder() const
     return m_fileName.Left(m_fileName.ReverseFind('\\'));
 }
 
+ULONGLONG FileProperties::getSize() const
+{
+    return m_sizeInBytes;
+}
+
+CTime FileProperties::getCreationTime() const
+{
+    return m_creationTime;
+}
+
+CTime FileProperties::getLastAccessTime() const
+{
+    return m_lastAccessTime;
+}
+
+CTime FileProperties::getLastWriteTime() const
+{
+    return m_lastWriteTime;
+}
+
 BOOL FileProperties::isDirectory() const
 {
     return m_isDirectory;
+}
+
+FileProperties::COMPARISON_RESULT FileProperties::makeChoice(ComparisonResults& results) const
+{
+    results.remove(COMPARISON_RESULT::EQUAL);
+
+    if (results.empty())
+    {
+        return COMPARISON_RESULT::EQUAL;
+    }
+
+    BOOL hasPreferable = std::find(results.begin(), results.end(), COMPARISON_RESULT::PREFERABLE) != results.end();
+    BOOL hasNonPreferable = std::find(results.begin(), results.end(), COMPARISON_RESULT::NON_PREFERABLE) != results.end();
+    
+    if (hasPreferable)
+    {
+        if (hasNonPreferable)
+        {
+            return COMPARISON_RESULT::UNDEFINED;
+        }
+        else
+        {
+            return COMPARISON_RESULT::PREFERABLE;
+        }
+    }
+    else
+    {
+        return COMPARISON_RESULT::NON_PREFERABLE;
+    }
 }
--- SimpleSync/FileProperties.h
+++ SimpleSync/FileProperties.h
@@ -1,12 +1,34 @@
 #pragma once
 
+#include <list>
 
+struct FileComparisonParameters;
 
 class FileProperties
 {
 public:
-    FileProperties(const CString& fileName = CString(), BOOL isDir = FALSE);
+
+    enum class COMPARISON_RESULT {
+        EQUAL,
+        PREFERABLE,
+        NON_PREFERABLE,
+        UNDEFINED
+    };
+    using ComparisonResults = std::list <COMPARISON_RESULT>;
+
+    FileProperties(const CString& fileName, ULONGLONG sizeInBytes,
+                   const CTime& creationTime, const CTime& accessTime, const CTime& writeTime,
+                   BOOL isDir = FALSE);
+    FileProperties(const CString& fileName, BOOL isDir = FALSE);
     ~FileProperties();
+
+    enum class TIMES {
+        CREATION_TIME,
+        LAST_WRITE_TIME,
+        LAST_ACCESS_TIME
+    };
+
+    COMPARISON_RESULT compareTo(const FileProperties& file, const FileComparisonParameters& params) const;
 
     BOOL operator< (const FileProperties& file) const;
     BOOL operator== (const FileProperties& file) const;
@@ -15,11 +37,52 @@ public:
     CString getFullPath() const;
     CString getFileFolder() const;
 
+    ULONGLONG getSize() const;
+
+    CTime getCreationTime() const;
+    CTime getLastAccessTime() const;
+    CTime getLastWriteTime() const;
+
     BOOL isDirectory() const;
 
 private:
+    COMPARISON_RESULT makeChoice(ComparisonResults& results) const;
+
+    template<class T>
+    COMPARISON_RESULT compareProperty(const T& first, const T& second) const;
+
     CString m_fileName;
+
+    ULONGLONG m_sizeInBytes;
+
+    CTime m_creationTime;
+    CTime m_lastAccessTime;
+    CTime m_lastWriteTime;
 
     BOOL m_isDirectory;
 };
 
+struct FileComparisonParameters
+{
+    BOOL compareSize = FALSE;
+    BOOL compareTime = FALSE;
+    FileProperties::TIMES timeToCompare = FileProperties::TIMES::LAST_WRITE_TIME;
+};
+
+template<>
+inline FileProperties::COMPARISON_RESULT FileProperties::compareProperty(const BOOL& first, const BOOL& second) const
+{
+    if (first || !second)
+        return COMPARISON_RESULT::PREFERABLE;
+    else
+        return first == second ? COMPARISON_RESULT::EQUAL : COMPARISON_RESULT::NON_PREFERABLE;
+}
+
+template<class T>
+FileProperties::COMPARISON_RESULT FileProperties::compareProperty(const T& first, const T& second) const
+{
+    if (first > second)
+        return COMPARISON_RESULT::PREFERABLE;
+    else
+        return first == second ? COMPARISON_RESULT::EQUAL : COMPARISON_RESULT::NON_PREFERABLE;
+}
